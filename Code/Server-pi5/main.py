b1738e36007b1b4f575862a25051f7ecd0725dcc
KarimAziev <karim.aziiev@gmail.com>
2024-11-05 18:47:27 +0200

    refactor: totally remove UI components in server

--- Code/Server-pi5/main.py
+++ Code/Server-pi5/main.py
@@ -1,10 +1,6 @@
 import logging
 import sys
 import time
-from ui_server import Ui_server
-from PyQt5.QtCore import QCoreApplication
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5 import QtGui
 import threading
 from Server import Server
 from typing import Optional
@@ -13,45 +9,24 @@ from typing import Optional
 logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s - %(levelname)s - %(message)s',
+    stream=sys.stdout,
 )
 
 logger = logging.getLogger(__name__)
 
 
-class MyWindow(QMainWindow, Ui_server):
-    def __init__(self, server: Server, user_ui: bool = True, start_tcp: bool = False):
-        super(MyWindow, self).__init__()
-        self.user_ui = user_ui
-        self.start_tcp = start_tcp
+class ServerController:
+    """
+    ServerController - controls the start and stop of the server and its threads
+    """
+
+    def __init__(self, server: Server, start_tcp: bool = False):
         self.server = server
         self.video_thread: Optional[threading.Thread] = None
         self.instruction_thread: Optional[threading.Thread] = None
 
-        logger.info("Server initialized")
-
-        if self.user_ui:
-            self.setupUi(self)
-            self.pushButton_On_And_Off.clicked.connect(self.on_and_off_server)
-            logger.debug("UI setup and button clicked signal connected")
-            self.on_and_off_server()
-
-        if self.start_tcp:
+        if start_tcp:
             self.start_server_threads()
-            if self.user_ui:
-                self.pushButton_On_And_Off.setText('Off')
-                self.states.setText('On')
-
-    def on_and_off_server(self):
-        if self.pushButton_On_And_Off.text() == 'On':
-            self.pushButton_On_And_Off.setText('Off')
-            self.states.setText('On')
-            self.start_server_threads()
-            logger.info("Server turned ON")
-        else:
-            self.pushButton_On_And_Off.setText('On')
-            self.states.setText('Off')
-            self.stop_server_threads()
-            logger.info("Server turned OFF")
 
     def start_server_threads(self):
         logger.info("Starting server threads")
@@ -61,6 +36,7 @@ class MyWindow(QMainWindow, Ui_server):
 
         self.video_thread = threading.Thread(target=self.server.transmission_video)
         self.video_thread.start()
+
         self.instruction_thread = threading.Thread(
             target=self.server.receive_instruction
         )
@@ -71,47 +47,43 @@ class MyWindow(QMainWindow, Ui_server):
         logger.info("Stopping server threads")
         self.server.tcp_flag = False
         self.server.stop_event.set()
+
         if self.video_thread and self.video_thread.is_alive():
             self.video_thread.join()
             logger.info("Video thread stopped")
+
         if self.instruction_thread and self.instruction_thread.is_alive():
             self.instruction_thread.join()
             logger.info("Instruction thread stopped")
-        self.server.turn_off_server()
-        logger.info("Server threads and sockets shut down")
 
-    def closeEvent(self, a0: QtGui.QCloseEvent):
-        logger.info("Close event triggered %s", a0)
-        self.stop_server_threads()
-        if self.user_ui:
-            QCoreApplication.instance().exit()
-            logger.info("Application exited")
+        self.server.turn_off_server()
+        logger.info("Server has been shut down gracefully.")
+
+    def run_forever(self):
+        """
+        Keeps the server running until interrupted.
+        """
+        logger.info("Running in non-UI mode.")
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            logger.info("KeyboardInterrupt received, stopping the server.")
+            self.stop_server_threads()
 
 
 if __name__ == '__main__':
     import argparse
 
-    parser = argparse.ArgumentParser(description="Run the application.")
-    parser.add_argument("-n", "--no-tcp", action="store_true", help="Disable TCP")
-    parser.add_argument("-u", "--ui", action="store_true", help="Allow UI")
+    parser = argparse.ArgumentParser(description="Run the server without UI.")
+    parser.add_argument(
+        "-n", "--no-tcp", action="store_true", help="Disable TCP completely"
+    )
 
     args = parser.parse_args()
-    user_ui = args.ui
     start_tcp = not args.no_tcp
 
     server = Server()
 
-    if user_ui:
-        app = QApplication(sys.argv)
-        window = MyWindow(server=server, user_ui=user_ui, start_tcp=start_tcp)
-        window.show()
-        logger.debug("UI shown")
-        sys.exit(app.exec_())
-    else:
-        window = MyWindow(server=server, user_ui=user_ui, start_tcp=start_tcp)
-        try:
-            while True:
-                time.sleep(1)
-        except KeyboardInterrupt:
-            logger.info("KeyboardInterrupt received, stopping server")
-            window.stop_server_threads()
+    controller = ServerController(server=server, start_tcp=start_tcp)
+    controller.run_forever()
