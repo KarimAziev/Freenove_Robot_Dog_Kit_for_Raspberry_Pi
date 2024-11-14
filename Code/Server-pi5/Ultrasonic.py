06f597f98f17f596a9bad40c7565bda9e8848eab
KarimAziev <karim.aziiev@gmail.com>
2024-11-14 16:03:36 +0200

    refactor: add pigpiod check for DistanceSensor initialization

--- Code/Server-pi5/Ultrasonic.py
+++ Code/Server-pi5/Ultrasonic.py
@@ -1,8 +1,38 @@
 from gpiozero import DistanceSensor
+from gpiozero.pins.pigpio import PiGPIOFactory
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def check_pigpiod():
+    """
+    Checks if pigpiod is running by attempting to connect with PiGPIOFactory.
+    If successful, returns an instance of PiGPIOFactory.
+    Otherwise, returns None.
+    """
+    try:
+
+        factory = PiGPIOFactory()
+        return factory
+    except (OSError, IOError):
+        logger.warning(
+            "Warning: pigpiod not running. Using default GPIOZero configuration."
+        )
+        return None
+
+
+factory = check_pigpiod()
 
 trigger_pin = 27
 echo_pin = 22
-sensor = DistanceSensor(echo=echo_pin, trigger=trigger_pin, max_distance=3)
+sensor = (
+    DistanceSensor(
+        echo=echo_pin, trigger=trigger_pin, max_distance=3, pin_factory=factory
+    )
+    if factory is not None
+    else DistanceSensor(echo=echo_pin, trigger=trigger_pin)
+)
 
 
 class Ultrasonic:
